29c4b99273b466318f48a42dfb3a7cd41c8e06af
Jakeh Clark <jakehclarkcreates@gmail.com>
2018-08-24 18:10:25 -0500

    post new comic works

--- server/public/scripts/controllers/entries.controller.js
+++ server/public/scripts/controllers/entries.controller.js
@@ -2,5 +2,77 @@
 
 ComicBookApp.controller('entriesController', ['$http', function ($http) {
     console.log('entriesController has been loaded');
+    vm = this;
+    vm.allComicEntries = [];
 
+
+    // Add new comics GET request, to display on DOM
+    vm.getComicEntries = function () {
+        console.log('In getComicEntries');
+
+        // request to server, will populate the allComicEntries array
+        $http({
+            method: 'GET',
+            url: '/entries/allComicEntries'
+        }).then(function (response) {
+            console.log('GET getComicEntries Working', response.data);
+            vm.allComicEntries = response.data
+        }).catch((error) => {
+            console.log('Error in get forSales', error);
+        })
+    };
+
+    vm.getComicEntries();
+
+   vm.nullifyThis = function(inputValue) {
+        if(inputValue == undefined) {
+            inputValue = null;
+            return inputValue;
+        } else {
+            inputValue = inputValue;
+            return inputValue;
+        };
+    }
+
+    vm.placeHolder = function(inputValue) {
+        if(inputValue == undefined || inputValue == null) {
+            inputValue = 'https://via.placeholder.com/150x250';
+            return inputValue;
+        } else {
+            inputValue = inputValue;
+            return inputValue;
+        };
+    }
+
+    // Add new comics POST request
+    vm.addNewComic = function () {
+        comicToAdd = {
+            title: vm.titleIn,
+            issue: vm.issueIn,
+            written_by: vm.writerIn,
+            art_by: vm.interiorArtIn,
+            cover_by: vm.coverArtIn,
+            comic_genre: vm.genreIn,
+            publisher: vm.publisherIn,
+            release_date: vm.releaseIn,
+            page_count: vm.pageCountIn,
+            image_url: vm.imageIn
+
+        }
+        
+        $http({
+            method: 'POST',
+            url: '/entries/postNewComic',
+            data: comicToAdd // req.body
+          }).then(function (response) {
+            console.log('addNewComic Working');
+            vm.getComicEntries();
+          }).catch(function (error) {
+            alert('addNewComic Not Working', error);
+            console.log('Error', error);
+          })
+
+        console.log(comicToAdd);
+
+    };
 }]);
--- server/routes/entries.route.js
+++ server/routes/entries.route.js
@@ -4,5 +4,54 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
+//GET Rentals
+router.get('/allComicEntries', function (req, res) {
+    console.log('In GET entries Route');
+    const query = `SELECT * FROM "comics" JOIN "genres" ON "comics"."comic_genre" = "genres"."id";
+    `;
+    pool.query(query).then((results) => {
+        //console.log(results);
+        res.send(results.rows);
+    }).catch((error) => {
+        console.log('Error getting allComicEntries', error);
+        res.sendStatus(500);
+    });
+}); //end GET
+
+router.post('/postNewComic', function (req, res) {
+    console.log('In postNewComic', req.body);
+    // Send req.body to postgres
+    // Add to database
+    const newComic = req.body; // data from $http
+
+    if (newComic.comic_genre == null || newComic.comic_genre == undefined) {
+
+        const query = `INSERT INTO "comics" ("title", "issue", "written_by", "art_by", "cover_by", "publisher", "release_date", "page_count", "image_url" )
+                   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);`;
+    pool.query(query, [newComic.title, newComic.issue, newComic.written_by, newComic.art_by, newComic.cover_by, newComic.publisher, newComic.release_date, newComic.page_count, newComic.image_url])
+        .then(() => {
+            res.sendStatus(200);
+        }).catch((error) => {
+            console.log('Error in /postNewComic POST request NULL/Undefined', error);
+            res.sendStatus(500);
+        });
+
+    } else {
+
+        const query = `INSERT INTO "comics" ("title", "issue", "written_by", "art_by", "cover_by", "comic_genre", "publisher", "release_date", "page_count", "image_url" )
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);`;
+        pool.query(query, [newComic.title, newComic.issue, newComic.written_by, newComic.art_by, newComic.cover_by, newComic.comic_genre, newComic.publisher, newComic.release_date, newComic.page_count, newComic.image_url])
+            .then(() => {
+                res.sendStatus(200);
+            }).catch((error) => {
+                console.log('Error in /postNewComic POST request valueDEfi', error);
+                res.sendStatus(500);
+            });
+    }
+
+
+});
+
+
 // Export module 
 module.exports = router;
