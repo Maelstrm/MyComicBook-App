950b96aa4b8349d7dcd4c4492125e14f2c039338
Jakeh Clark <jakehclarkcreates@gmail.com>
2018-08-25 21:28:51 -0500

    refactored buttons and added logic to dom elements.

--- server/public/scripts/controllers/entries.controller.js
+++ server/public/scripts/controllers/entries.controller.js
@@ -16,29 +16,30 @@ ComicBookApp.controller('entriesController', ['$http', function ($http) {
             url: '/entries/allComicEntries'
         }).then(function (response) {
             console.log('GET getComicEntries Working', response.data);
+            // Store all Comics from database in a local variable
             vm.allComicEntries = response.data;
+
+            // Loops through all comics.
+            // If date is null or undefined, it will be ignored.
+            // If if there is a date, then it will be conversted using moment.js
             for (let i = 0; i < vm.allComicEntries.length; i++) {
-                vm.allComicEntries[i].release_date = moment(vm.allComicEntries[i].release_date).format('MMMM Do YYYY');
+                if (vm.allComicEntries[i].release_date == null || undefined) {
+                    return;
+                } else {
+                    vm.allComicEntries[i].release_date = moment(vm.allComicEntries[i].release_date).format('MMMM Do YYYY');
+                }
             }
         }).catch((error) => {
             console.log('Error in get forSales', error);
         })
     };
 
+    // Initial GET request when dom is loaded
     vm.getComicEntries();
 
-   vm.nullifyThis = function(inputValue) {
-        if(inputValue == undefined) {
-            inputValue = null;
-            return inputValue;
-        } else {
-            inputValue = inputValue;
-            return inputValue;
-        };
-    }
-
-    vm.placeHolder = function(inputValue) {
-        if(inputValue == undefined || inputValue == null) {
+    // If the image field is left blank, then this placeholder image will be used instead.
+    vm.placeHolder = function (inputValue) {
+        if (inputValue == undefined || inputValue == null) {
             inputValue = 'https://via.placeholder.com/150x250';
             return inputValue;
         } else {
@@ -60,20 +61,29 @@ ComicBookApp.controller('entriesController', ['$http', function ($http) {
             release_date: vm.releaseIn,
             page_count: vm.pageCountIn,
             image_url: vm.placeHolder(vm.imageIn)
-
         }
-        
         $http({
             method: 'POST',
             url: '/entries/postNewComic',
             data: comicToAdd // req.body
-          }).then(function (response) {
+        }).then(function (response) {
             console.log('addNewComic Working');
             vm.getComicEntries();
-          }).catch(function (error) {
+        }).catch(function (error) {
             alert('addNewComic Not Working', error);
             console.log('Error', error);
-          })
+        })
 
     };
+
+    // Remove comic DELETE request
+    // The GET requests makes the id = to the genre, because that is where the join occures.
+    // Because of this I have to perform the deletion using some other relevant data.
+    // Multiple books shouldnt have the same values for these three properties
+    vm.deleteComic = function(comicTitle, comicId, comicIssue) {
+        console.log('in Delete Comic for', comicTitle, comicId, comicIssue);
+        
+        
+
+    }
 }]);
--- server/routes/entries.route.js
+++ server/routes/entries.route.js
@@ -24,20 +24,22 @@ router.post('/postNewComic', function (req, res) {
     // Add to database
     const newComic = req.body; // data from $http
 
+    // Since the the joined table is connected with the comic genre column, it cannot be entered in the database as null or undefined.
+    // If this happens, then it will be left out of the query.
+    // Postgresql will set undefined/null genre to 'NA', which will not show up on the dom
     if (newComic.comic_genre == null || newComic.comic_genre == undefined) {
-
         const query = `INSERT INTO "comics" ("title", "issue", "written_by", "art_by", "cover_by", "publisher", "release_date", "page_count", "image_url" )
                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);`;
-    pool.query(query, [newComic.title, newComic.issue, newComic.written_by, newComic.art_by, newComic.cover_by, newComic.publisher, newComic.release_date, newComic.page_count, newComic.image_url])
-        .then(() => {
-            res.sendStatus(200);
-        }).catch((error) => {
-            console.log('Error in /postNewComic POST request NULL/Undefined', error);
-            res.sendStatus(500);
-        });
-
-    } else {
-
+        pool.query(query, [newComic.title, newComic.issue, newComic.written_by, newComic.art_by, newComic.cover_by, newComic.publisher, newComic.release_date, newComic.page_count, newComic.image_url])
+            .then(() => {
+                res.sendStatus(200);
+            }).catch((error) => {
+                console.log('Error in /postNewComic POST request NULL/Undefined', error);
+                res.sendStatus(500);
+            });
+    }
+    // If the genre is selected from the drop-down table, then the new item will be sent to the database with all fields
+    else {
         const query = `INSERT INTO "comics" ("title", "issue", "written_by", "art_by", "cover_by", "comic_genre", "publisher", "release_date", "page_count", "image_url" )
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);`;
         pool.query(query, [newComic.title, newComic.issue, newComic.written_by, newComic.art_by, newComic.cover_by, newComic.comic_genre, newComic.publisher, newComic.release_date, newComic.page_count, newComic.image_url])
@@ -48,8 +50,6 @@ router.post('/postNewComic', function (req, res) {
                 res.sendStatus(500);
             });
     }
-
-
 });
 
 
